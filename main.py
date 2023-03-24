233c5e1353e8373972352abcc7fd94062f1ca430
saiganesh08 <93111749+saiganesh08@users.noreply.github.com>
2023-03-24 20:54:45 +0530

    Update main.py
    Accepts the user input

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import subprocess
 compiler = Tk()
 compiler.title('My PYTHON IDE')
 file_path = ''
+external_libraries = []
+user_input = None
 
 
 def set_file_path(path):
@@ -32,23 +34,39 @@ def save_as():
         set_file_path(path)
 
 
+def add_library():
+    lib_path = askopenfilename(filetypes=[('Python Files', '*.py')])
+    if lib_path:
+        external_libraries.append(lib_path)
+
+
+def get_user_input():
+    global user_input
+    user_input = input_box.get('1.0', END)
+
+
 def run():
     if file_path == '':
         save_prompt = Toplevel()
         text = Label(save_prompt, text='Please save your code')
         text.pack()
         return
-    external_libraries = ['numpy', 'pandas', 'matplotlib', 'scikit-learn','tkinter' ]
 
+    command = f'python {file_path}'
+    if external_libraries:
+        command += ' ' + ' '.join(external_libraries)
 
-    libraries = ' '.join(['-i ' + lib for lib in external_libraries]) # add all external libraries to command line arguments
+    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               shell=True)
 
-    command = f'python {file_path} {libraries}' # include libraries as an argument
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    # Pass user input to the process
+    if user_input:
+        process.stdin.write(user_input.encode())
+        process.stdin.flush()
+
     output, error = process.communicate()
-    code_output.insert('1.0', output)
-    code_output.insert('1.0',  error)
-
+    code_output.insert('1.0', output.decode())
+    code_output.insert('1.0', error.decode())
 
 
 menu_bar = Menu(compiler)
@@ -60,14 +78,24 @@ file_menu.add_command(label='Save As', command=save_as)
 file_menu.add_command(label='Exit', command=exit)
 menu_bar.add_cascade(label='File', menu=file_menu)
 
-run_bar = Menu(menu_bar, tearoff=0)
-run_bar.add_command(label='Run', command=run)
-menu_bar.add_cascade(label='Run', menu=run_bar)
+run_menu = Menu(menu_bar, tearoff=0)
+run_menu.add_command(label='Run', command=run)
+menu_bar.add_cascade(label='Run', menu=run_menu)
+
+library_menu = Menu(menu_bar, tearoff=0)
+library_menu.add_command(label='Add library', command=add_library)
+menu_bar.add_cascade(label='External Libraries', menu=library_menu)
 
 compiler.config(menu=menu_bar)
 
 editor = Text()
 editor.pack()
+
+input_box = Text(height=5)
+input_box.pack()
+
+run_button = Button(text='Run', command=lambda: [get_user_input(), run()])
+run_button.pack()
 
 code_output = Text(height=10)
 code_output.pack()
